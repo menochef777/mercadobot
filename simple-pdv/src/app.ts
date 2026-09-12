cc13d982da099b4aa515d2340b7e088fdb23b99f
menochef777 <menochef777@users.noreply.github.com>
2026-09-12 17:35:29 -0300

    perf(express): aumentar limite do body parser para 50mb (json e urlencoded)

--- simple-pdv/src/app.ts
+++ simple-pdv/src/app.ts
@@ -28,7 +28,8 @@ const app = express()
 setupSwagger(app);
 app.use('/files', express.static('files'))
 app.use(morgan('dev'))
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cors())
 
 app.use('/login', Login)
